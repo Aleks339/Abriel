c85118ab5784259e74c022c100c13353bd6b506e
Aleks339 <annamuljar9@gmail.com>
2018-02-01 15:40:00 +0300

    добавлены фон, формы и меню

---
+++ action.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Здравствуйте, <?php echo $_POST["name"]; ?><br>
+Ваш телефон: <?php echo $_POST["tel"]; ?> <br>
+Ваш e-mail: <?php echo $_POST["email"]; ?> <br>
+Ваш комментарий: <?php echo $_POST["comment"]; ?> 
+
+</body>
+</html>
